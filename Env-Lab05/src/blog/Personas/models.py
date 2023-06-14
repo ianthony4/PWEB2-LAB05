4cc6e79ebaa07ded8ca8b842b6d92bf4dd507e9c
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-06-13 22:36:19 -0500

    Creando los espacios para Persona

--- Env-Lab05/src/blog/Personas/models.py
+++ Env-Lab05/src/blog/Personas/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    #Opciones para elegir el genero
+    GENERO_CHOICES = [
+        ('H', 'Hombre'),
+        ('M', 'Mujer'),
+        ('ND', 'Prefiero no decirlo')
+    ]
+
+    #Espacios para el registro de personas
+    usuario = models.CharField(max_length=100)
+    nombres = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    edad = models.IntegerField()
+    genero = models.CharField(max_length=2, choices=GENERO_CHOICES)
+    ciudad = models.CharField(max_length=100)
+    correo = models.CharField(max_length=100)
+    
+    # Esto mostrara el usuario en vez de 'persona object'
+    def __str__(self):
+        return self.usuario
