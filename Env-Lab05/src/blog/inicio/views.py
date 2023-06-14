9b89b6e61203644719323746aa949d4d7d311f09
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-06-13 23:13:40 -0500

    Añadiendo inicio a setting y a view

--- Env-Lab05/src/blog/inicio/views.py
+++ Env-Lab05/src/blog/inicio/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def myHomeView(*args, **kwargs):
+    return HttpResponse('<h1>Pagina de inicio en django</h1>')
